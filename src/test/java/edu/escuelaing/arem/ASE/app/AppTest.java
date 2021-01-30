115b07f982fe1322261d4a83dad95e4af063a4a9
GYEISSON <yeison7f@gmail.com>
2021-01-29 23:18:32 -0500

    pruebas realizadas

--- src/test/java/edu/escuelaing/arem/ASE/app/AppTest.java
+++ src/test/java/edu/escuelaing/arem/ASE/app/AppTest.java
@@ -18,21 +18,33 @@ public class AppTest
     public AppTest( String testName )
     {
         super( testName );
+        LinkedList lista = new LinkedList();
+        lista.addFirs(159.847);
+        lista.addFirs(0.379);lista.addFirs(753);lista.addFirs(15.5);
+        lista.addFirs(33.5);lista.addFirs(88.78);lista.addFirs(99.0);
+        assertEquals(99.0, ((Nodo) lista.removeFirst()).getData(),0.1);
+        assertEquals(88.78, ((Nodo) lista.removeFirst()).getData(),0.01);
+        assertEquals(33.5, ((Nodo) lista.removeFirst()).getData(),0.1);
+        assertEquals(15.5, ((Nodo) lista.removeFirst()).getData(),0.1);
+        assertEquals(753, ((Nodo) lista.removeFirst()).getData(),0.1);
+        assertEquals(0.379, ((Nodo) lista.removeFirst()).getData(),0.001);
     }
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
+
 
     /**
      * Rigourous Test :-)
      */
     public void testApp()
     {
-        assertTrue( true );
+    	 LinkedList lista = new LinkedList();
+         lista.addFirs(159.847);
+         lista.addFirs(0.379);lista.addFirs(753);lista.addFirs(15.5);
+         lista.addFirs(33.5);lista.addFirs(88.78);lista.addFirs(99.0);
+         double mean = App.mean(lista);
+         double deviation = App.standardDeviation(lista);
+         
+         assertEquals( 164.286,mean,0.001);	
+         assertEquals(245.727,deviation, 0.001);
     }
 }
